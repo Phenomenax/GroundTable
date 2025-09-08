83af059d18b3ec9353b527cab9592da4c5b46f0e
JoeLi <liyuanzhuo0704@gmail.com>
2025-09-08 21:41:28 +1000

    remove unnecessary code

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,25 +1,21 @@
 import Link from "next/link";
 
 import { auth } from "~/server/auth";
-import { HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
-  // const hello = await api.post.hello({ text: "from tRPC" });
   const session = await auth();
 
   return (
-    <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center">
-        <div className="flex flex-col items-center justify-center gap-4">
-          <p>Logged in as {session?.user.email}</p>
-          <Link
-            href={"/api/auth/signout"}
-            className="rounded-full bg-black text-white px-10 py-3 font-semibold no-underline transition hover:bg-black/90"
-          >
-            {"Sign out"}
-          </Link>
-        </div>
-      </main>
-    </HydrateClient>
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <p>Logged in as {session?.user.email}</p>
+        <Link
+          href={"/api/auth/signout"}
+          className="rounded-full bg-black text-white px-10 py-3 font-semibold no-underline transition hover:bg-black/90"
+        >
+          {"Sign out"}
+        </Link>
+      </div>
+    </main>
   );
 }
